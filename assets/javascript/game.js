f511a4c0ad68f09f475e0bf9c3e0cbe899b40d9c
Melanie Bott <melaniebott@gmail.com>
2020-03-29 19:48:29 -0600

    after submitting the game wasn't working properly. Went back and made corrections. the innderHTML was causing problems. Working now

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -9,7 +9,7 @@ $(document).ready(function() {
     function randomNum(min, max) {
         min = Math.ceil(min);
         max = Math.floor(max);
-        return Math.floor(Math.random() * max - min + 1) + min;
+        return Math.floor(Math.random() * (max - min + 1)) + min;
     }
 
     var targetNumber = randomNum(19, 120);
@@ -20,10 +20,10 @@ $(document).ready(function() {
     var yellowCrayon = randomNum(1, 12);
     var purpleCrayon = randomNum(1, 12);
 
-    function reset() {
+    function resetGame() {
         counter = 0;
-        $("#target").html(targetNumber);
         targetNumber = randomNum(19, 120);
+        $("#target").html(targetNumber);
         $("#playerScore").text(counter);
         redCrayon = randomNum(1, 12);
         blueCrayon = randomNum(1, 12);
@@ -33,41 +33,49 @@ $(document).ready(function() {
 
     $("#redCrayon").on("click", function() {
         counter += redCrayon;
-        WinLoose();
         $(playerScore).text(counter);
+        WinLoose();
     });
 
     $("#blueCrayon").on("click", function() {
         counter += blueCrayon;
-        WinLoose();
         $(playerScore).text(counter);
+        WinLoose();
     });
 
     $("#yellowCrayon").on("click", function() {
         counter += yellowCrayon;
-        WinLoose();
         $(playerScore).text(counter);
+        WinLoose();
     });
 
     $("#purpleCrayon").on("click", function() {
         counter += purpleCrayon;
-        WinLoose();
         $(playerScore).text(counter);
+        WinLoose();
     });
 
     function WinLoose() {
-
         if (counter === targetNumber) {
-            wins += 1;
-            alert("Winner!");
-            $(win).text(wins);
-            reset();
+            console.log("victory");
+            document.getElementById("winSound").play();
+            wins++;
+            // $(win).text(wins);
+            updateDisplay();
+            setTimeout(resetGame, -1);
 
         } else if (counter > targetNumber) {
-            losses += 1;
-            alert("Not good enough. You LOOSE!");
-            $(loss).text(losses);
-            reset();
+            document.getElementById("loseSound").play();
+            losses++;
+            // $(loss).text(losses);
+            updateDisplay();
+            setTimeout(resetGame, -1);
+            // reset();
         }
+    }
+
+    function updateDisplay() {
+        document.getElementById("win").innerHTML = wins;
+        document.getElementById("loss").innerHTML = losses;
     }
 });
